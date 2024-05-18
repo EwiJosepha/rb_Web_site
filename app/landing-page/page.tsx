de72b5add19859b02b7d851d1d754303a309b1a9
ewi josepha <ewijosepha@gmail.com>
2024-05-18 16:38:38 +0200

    origin section

--- app/landing-page/page.tsx
+++ app/landing-page/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "../components/organisms/hero"
 import Navbar from "../components/organisms/navbar"
+import OriginSec from "../components/organisms/origin"
 function Landing() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <OriginSec />
 
     </div>
   )
