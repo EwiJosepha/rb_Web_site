b8293a7c8f7ebca73774f84222d39148551a698b
ewi josepha <ewijosepha@gmail.com>
2024-05-21 12:56:06 +0200

    adding contact component

---
+++ app/components/organisms/faq.tsx
@@ -0,0 +1,223 @@
+import React from "react";
+import QuizPicture from "./quix-pic";
+import { playfair } from "@/public/fonts";
+
+function Faqs() {
+    // const requiremts = [
+    //   {
+    //     question: "What are the prerequisites to join Rebase Code Camp?",
+    //     requirement1: "At least a GCE A-level certificate or equivalent qualification",
+    //     requirement2: "Your personal computer (laptop)",
+    //     requirement3: "Stable internet at home for home studies (the institution provides internet connection on campus.)",
+    //   },
+    //   {
+    //     question: "What is the teaching approach at Rebase Code Camp?",
+    //     answer: " Rebase Code Camp follows a hands-on and project-based learning approach. Students work on real-world projects to gain practical experience and develop their skills.",
+    //     requirement1: "At least a GCE A-level certificate or equivalent qualification",
+    //     requirement2: "Your personal computer (laptop)",
+    //     requirement3: "Stable internet at home for home studies (the institution provides internet connection on campus.)",
+    //   },
+    //   {
+    //     question: "What are the prerequisites to join Rebase Code Camp?",
+    //     answer: "Rebase Code Camp does not provide certificates. However, we offer possibility for students to obtain internationally recognised certificates that can be a valuable addition to your resume and showcase your skills to potential employers.",
+    //     requirement1: "At least a GCE A-level certificate or equivalent qualification",
+    //     requirement2: "Your personal computer (laptop)",
+    //     requirement3: "Stable internet at home for home studies (the institution provides internet connection on campus.)",
+    //   },
+    //   {
+    //     question: "What are the prerequisites to join Rebase Code Camp?",
+    //     answer: "Rebase Code Camp offers training in various programming languages, with main focus on MERN stack",
+    //     requirement1: "At least a GCE A-level certificate or equivalent qualification",
+    //     requirement2: "Your personal computer (laptop)",
+    //     requirement3: "Stable internet at home for home studies (the institution provides internet connection on campus.)",
+    //   }
+    // ]
+    return (
+        <>
+            <div>
+                <div className=" mobile:max-md:ml-[1rem]mobile:max-md:pl-5   h-full bg-white pl-20 pr-20 pt-20  mobile:max-md:mr-[-9rem] mobile:max-md:px-5 mobile:max-md:pr-5 mobile:max-md:pt-10">
+                    <h1
+                        className={` ${playfair.className} pb-10  pt-8 text-center text-6xl mobile:max-md:text-3xl`}
+                    >
+                        FAQs
+                    </h1>
+                    <p className=" pb-10 text-center font-sans text-lg ">
+                        Discover and learn from our frequently asked questions
+                    </p>
+                </div>
+                <div className="grid grid-cols-2 justify-center gap-10 pl-20 pr-20 align-middle  mobile:max-md:mr-[-9rem] mobile:max-md:grid-cols-1 mobile:max-md:gap-4 mobile:max-md:pl-5 mobile:max-md:pr-5">
+                    <div
+                        className="  rounded-lx p-3 py-3 mobile:max-md:w-[100%]"
+                        style={{ height: "100%", background: "#efefef" }}
+                    >
+                        <h2 className=" text-1xl pb-2 pt-2 text-center font-sans font-bold mobile:max-md:text-lg">
+                            What are the prerequisites to join Rebase Code Camp?
+                        </h2>
+                        <p className=" pb-1 pl-5 pr-5 text-center font-sans text-lg text-gray-500 mobile:max-md:text-lg">
+                            We train people with or without a background in
+                            programming. To study with us, all you need are:
+                        </p>
+                        <div className="">
+                            <ul className="list-disc pl-10 text-xs text-gray-500">
+                                <li className=" pl-5 font-sans text-lg mobile:max-md:text-lg">
+                                    At least a GCE A-level certificate or
+                                    equivalent qualification
+                                </li>
+                                <li className="pl-5 font-sans text-lg mobile:max-md:text-lg">
+                                    Your personal computer (laptop)
+                                </li>
+
+                                <li className=" pl-5 font-sans text-lg mobile:max-md:text-lg">
+                                    Stable internet at home for home studies
+                                    (the institution provides internet
+                                    connection on campus.)
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div
+                        className="  rounded-lx py-3"
+                        style={{ height: "100%", background: "#efefef" }}
+                    >
+                        <h2 className="text-1xl pb-2 pt-2 text-center font-sans font-bold mobile:max-md:text-lg">
+                            What is the teaching approach at Rebase Code Camp?
+                        </h2>
+                        <div className="">
+                            <p className=" pl-8 pr-8 text-center font-sans text-lg text-gray-500 mobile:max-md:text-lg">
+                                Rebase Code Camp follows a hands-on and
+                                project-based learning approach. Students work
+                                on real-world projects to gain practical
+                                experience and develop their skills.
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        className="  rounded-lx py-3"
+                        style={{ height: "100%", background: "#efefef" }}
+                    >
+                        <h2 className="text-1xl pb-2 pt-2 text-center font-sans font-bold mobile:max-md:text-lg">
+                            What are the prerequisites to join Rebase Code Camp?
+                        </h2>
+                        <div className="">
+                            <p className=" pl-8 pr-8 text-center font-sans text-lg text-gray-500 mobile:max-md:text-lg">
+                                Rebase Code Camp does not provide certificates.
+                                However, we offer possibility for students to
+                                obtain internationally recognised certificates
+                                that can be a valuable addition to your resume
+                                and showcase your skills to potential employers.
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        className="  rounded-lx py-3"
+                        style={{ height: "100%", background: "#efefef" }}
+                    >
+                        <h2 className="text-1xl pb-2 pt-2 text-center font-sans font-bold mobile:max-md:text-lg">
+                            What are the prerequisites to join Rebase Code Camp?
+                        </h2>
+                        <div className="">
+                            <p className=" pl-8 pr-8 text-center font-sans text-lg text-gray-500 mobile:max-md:text-lg">
+                                Rebase Code Camp offers training in various
+                                programming languages, with main focus on MERN
+                                stack.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <QuizPicture />
+            <div className="grid grid-cols-2 justify-center gap-10 pl-20 pr-20 align-middle  mobile:max-md:mr-[-9rem] mobile:max-md:grid-cols-1 mobile:max-md:gap-4 mobile:max-md:pl-5 mobile:max-md:pr-5 mobile:max-md:pt-10">
+                <div
+                    className="  rounded-lx p-2 py-3 pr-5"
+                    style={{ height: "100%", background: "#efefef" }}
+                >
+                    <h2 className="text-1xl pb-2 pt-2 text-center font-sans font-bold mobile:max-md:text-lg">
+                        What are the prerequisites to join Rebase Code Camp?
+                    </h2>
+                    <p className=" pb-3 pl-5 pr-5 text-center font-sans text-lg text-gray-500 mobile:max-md:text-lg">
+                        We train people with or without a background in
+                        programming. To study with us, all you need are:
+                    </p>
+                    <div className="">
+                        <ul className="list-disc pl-10 text-xs text-gray-500">
+                            <li className=" pl-5 font-sans  text-lg mobile:max-md:text-lg">
+                                At least a GCE A-level certificate or equivalent
+                                qualification
+                            </li>
+                            <li className=" pl-5 font-sans text-lg mobile:max-md:text-lg">
+                                Your personal computer (laptop)
+                            </li>
+
+                            <li className=" pl-5 font-sans text-lg mobile:max-md:text-lg">
+                                Stable internet at home for home studies (the
+                                institution provides internet connection on
+                                campus.)
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div
+                    className="  rounded-lx py-3 pr-5"
+                    style={{ height: "100%", background: "#efefef" }}
+                >
+                    <h2 className=" text-1xl pb-2 pt-2 text-center font-sans font-bold mobile:max-md:pl-1 mobile:max-md:text-lg">
+                        What are the prerequisites to join Rebase Code Camp?
+                    </h2>
+                    <p className=" pb-3 pl-5 pr-5 text-center font-sans text-lg text-gray-500 mobile:max-md:text-lg">
+                        We train people with or without a background in
+                        programming. To study with us, all you need are:
+                    </p>
+                    <div className=" font-sans">
+                        <ul className="list-disc pl-10 text-xs text-gray-500">
+                            <li className=" pl-5 text-lg mobile:max-md:text-lg">
+                                At least a GCE A-level certificate or equivalent
+                                qualification
+                            </li>
+                            <li className="pl-5 font-sans text-lg mobile:max-md:text-lg">
+                                Your personal computer (laptop)
+                            </li>
+
+                            <li className=" pl-5 font-sans text-lg mobile:max-md:text-lg">
+                                Stable internet at home for home studies (the
+                                institution provides internet connection on
+                                campus.)
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div
+                    className="  rounded-lx py-3"
+                    style={{ height: "100%", background: "#efefef" }}
+                >
+                    <h2 className=" text-1xl pb-2 pt-2 text-center font-sans font-bold mobile:max-md:pl-1 mobile:max-md:text-lg">
+                        What are the prerequisites to join Rebase Code Camp?
+                    </h2>
+                    <div className="">
+                        <p className=" pl-8 pr-8 text-center font-sans text-lg text-gray-500 mobile:max-md:text-lg">
+                            Rebase Code Camp offers training in various
+                            programming languages, with main focus on MERN
+                            stack.
+                        </p>
+                    </div>
+                </div>
+                <div
+                    className="  rounded-lx py-3"
+                    style={{ height: "100%", background: "#efefef" }}
+                >
+                    <h2 className=" text-1xl pb-2 pt-2 text-center font-sans font-bold mobile:max-md:text-lg">
+                        What are the prerequisites to join Rebase Code Camp?
+                    </h2>
+                    <div className="">
+                        <p className=" pl-8 pr-8 text-center font-sans text-lg text-gray-500 mobile:max-md:text-lg">
+                            Rebase Code Camp offers training in various
+                            programming languages, with main focus on MERN
+                            stack.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Faqs;
---
+++ app/components/organisms/testimonial.tsx
@@ -0,0 +1,98 @@
+import Image from "next/image";
+import React from "react";
+
+import dp from "@/public/assets/images/photo_placement_720.png";
+import { GrNext, GrPrevious } from "react-icons/gr";
+import { FaStar } from "react-icons/fa6";
+// import { playfair } from "@/public/fonts";
+
+const Testimonials: React.FC = () => {
+    const testifier = [
+        {
+            id: 1,
+            image: dp,
+            title: "How to manage SEO as a client",
+            date: "June 24th, 2024",
+            owner: "John Mary",
+            text: "Rebase Academy has truly transformed my career. The hands-on learning experience and industry-relevant curriculum prepared me for success in the tech job market",
+        },
+        {
+            id: 2,
+            image: dp,
+            title: "How to manage SEO as a client",
+            date: "June 24th, 2024",
+            owner: "John Doe",
+            text: "Rebase Academy has truly transformed my career. The hands-on learning experience and industry-relevant curriculum prepared me for success in the tech job markett",
+        },
+        {
+            id: 3,
+            image: dp,
+            title: "How to manage SEO as a client",
+            date: "June 24th, 2024",
+            owner: "solan Doe",
+            text: "Rebase Academy has truly transformed my career. The hands-on learning experience and industry-relevant curriculum prepared me for success in the tech job markett",
+        },
+    ];
+
+    return (
+        <div className="mb-14 mobile:max-md:mx-5 mobile:max-md:mr-[-9rem] lg:mx-36">
+            <h1
+                className={` mb-6 text-center text-6xl mobile:max-md:text-3xl`}
+            >
+                Testimonials
+            </h1>
+            <h3 className="mb-10 text-center font-sans text-xl">
+                Hear what our students, former students and industry
+                professionals have to say
+            </h3>
+            <div className="flex items-center justify-around">
+                <button className=" left-7 -translate-y-1/2 transform rounded-full bg-orange-500 px-5 py-5 font-bold mobile:max-md:px-2 mobile:max-md:py-2">
+                    <GrPrevious className=" previousbtn text-2xl text-white" />
+                </button>
+
+                <div className="grid w-[75%] grid-cols-2 gap-10 overflow-hidden">
+                    {testifier.slice(0, 2).map((testimony, index) => (
+                        <div
+                            className="card bg-gray-200 p-10"
+                            key={index}
+                            style={{}}
+                        >
+                            <div className="mb-4 flex">
+                                <FaStar className="text-xl" />
+                                <FaStar className="text-xl" />
+                                <FaStar className="text-xl" />
+                                <FaStar className="text-xl" />
+                                <FaStar className="text-xl" />
+                            </div>
+
+                            <h2 className="font-sans text-lg font-bold mobile:max-md:hidden">
+                                {testimony.text}.
+                            </h2>
+                            <div className="my-8 h-24 w-20">
+                                <Image
+                                    src={dp}
+                                    alt="dp"
+                                    className="h-full w-full rounded-t-full border border-gray-200 object-cover"
+                                />
+                            </div>
+                            <h2 className="mb-2 text-lg font-semibold">
+                                {testimony.owner}
+                            </h2>
+                            <p className=" mobile:max-md:hidden">
+                                Software Engineer, xyz company.
+                            </p>
+                        </div>
+                    ))}
+                </div>
+
+                <button className=" left-7 -translate-y-1/2 transform rounded-full bg-orange-500 px-5 py-5 font-bold mobile:max-md:px-2 mobile:max-md:py-2">
+                    <GrNext
+                        className="next-button
+                    text-2xl text-white"
+                    />
+                </button>
+            </div>
+        </div>
+    );
+};
+export default Testimonials;
