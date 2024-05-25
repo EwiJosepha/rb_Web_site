d76bf65e7e7b5554d0e5e2224d795722e0198c74
ewi josepha <ewijosepha@gmail.com>
2024-05-25 16:24:58 +0200

    adding a contact route and component

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../components/organisms/footer";
+import AboutUs from "../components/organisms/about-us";
+
+function AboutPage() {
+    return (
+        <div className=" py-20">
+            <AboutUs />
+            <Footer />
+        </div>
+    );
+}
+
+export default AboutPage;
