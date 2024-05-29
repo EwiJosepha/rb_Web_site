beb4a40527df13b2283e69ea7be7ac2c2c4861c9
ewi josepha <ewijosepha@gmail.com>
2024-05-29 15:45:21 +0200

    adding team to contac us route

--- app/components/contact/page.tsx
+++ app/components/contact/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ContactUs from "../organisms/contact";
 import Footer from "../organisms/footer";
+import Team from "../organisms/team";
 
 function ContactPage() {
     return (
         <div className=" py-20">
             <ContactUs />
+            <Team />
             <Footer />
         </div>
     );
